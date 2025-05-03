e9a03ba9dbc48b9ec21eae132bb237a6bd040743
std-1224 <kalkaudy@gmail.com>
2025-05-04 00:53:52 +0700

    add env for gmail

--- app/config/env.ts
+++ app/config/env.ts
@@ -4,3 +4,5 @@ export const GOOGLE_MAPS_API_KEY = 'AIzaSyBC5QKPsheGa9b9xd_p0h3Ci5gNUx9sGL8';
 export const MAP_BOX_TOKEN = '';
 export const MAPBOX_TOKEN =
   'pk.eyJ1IjoiZXJpY2Nhc3M1IiwiYSI6ImNtMW1hbzZ5ajBqZ2kycnEwenZkZHc4eDIifQ.ng3x57hHiP_WkjVG1UQ-Tw';
+export const MY_EMAIL = 'kalkaudy@gmail.com';
+export const MY_PASSWORD = 'innerpeace1238';
