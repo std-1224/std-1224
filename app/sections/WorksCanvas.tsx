515870132becde732dc68c6956491249bd21212e
std-1224 <kalkaudy@gmail.com>
2025-05-05 00:44:01 +0700

    scrollbarwidth none in works

--- app/sections/WorksCanvas.tsx
+++ app/sections/WorksCanvas.tsx
@@ -4,7 +4,7 @@ import '../util'
 import { Banner, Carousel, Rig } from '../components/CardThree'
 const WorksCanvas = () => {
   return (
-    <Canvas camera={{ position: [0, 0, 100], fov: 15 }}  >
+    <Canvas camera={{ position: [0, 0, 100], fov: 15 }}  style={{scrollbarWidth: 'none'}}>
       <fog attach="fog" args={['#a79', 8.5, 12]} />
       <ScrollControls pages={10} infinite style={{scrollbarWidth: 'none'}}>
         <Rig rotation={[0, 0, 0.15]}>
